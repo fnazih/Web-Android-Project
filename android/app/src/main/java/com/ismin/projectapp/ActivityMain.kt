43122b371df5de187a96aab4fa317c3716aca18d
Fatima-Zohra Nazih <fnazih03@gmail.com>
2020-12-03 17:09:44 +0100

    finished design

--- android/app/src/main/java/com/ismin/projectapp/ActivityMain.kt
+++ android/app/src/main/java/com/ismin/projectapp/ActivityMain.kt
@@ -33,10 +33,21 @@ class ActivityMain: AppCompatActivity(), ExpandableListView.OnChildClickListener
     private val favorites = EventList()
     private var fragment: SubCategoryFragment = SubCategoryFragment()
     private lateinit var eventService: EventService
+    private lateinit var myButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myButton = findViewById(R.id.favoritesButt)
+        myButton.setOnClickListener {
+            if (myButton.text == "Favoris") {
+                goToFavorites()
+            }
+            else {
+                displayEventList()
+            }
+        }
 
         val retrofit = Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
@@ -60,52 +71,6 @@ class ActivityMain: AppCompatActivity(), ExpandableListView.OnChildClickListener
                 Toast.makeText(applicationContext, "Network error", Toast.LENGTH_LONG).show()
             }
         })
-
-        //updateList()
-        //expandableListView = findViewById(R.id.expendableListView)
-        //listViewAdapter = ExpendableListViewAdapter(this, categories, subCategories)
-        //expandableListView.setAdapter(listViewAdapter)
-        //expandableListView.setOnChildClickListener(this)
-    }
-
-    private fun updateList() {      //fct a adapter qd on aura la bdd
-        categories = ArrayList()
-        subCategories = HashMap()
-
-        //Test de la liste avec des elements au hasard
-        categories.add("Categorie 1")
-        categories.add("Categorie 2")
-        categories.add("Categorie 3")
-        categories.add("Categorie 4")
-
-        val subCategory1: ArrayList<String> = ArrayList()
-        subCategory1.add("SubCategory1")
-        subCategory1.add("SubCategory2")
-        subCategory1.add("SubCategory3")
-        subCategory1.add("SubCategory4")
-
-        val subCategory2: ArrayList<String> = ArrayList()
-        subCategory2.add("SubCategory1")
-        subCategory2.add("SubCategory2")
-        subCategory2.add("SubCategory3")
-        subCategory2.add("SubCategory4")
-
-        val subCategory3: ArrayList<String> = ArrayList()
-        subCategory3.add("SubCategory1")
-        subCategory3.add("SubCategory2")
-        subCategory3.add("SubCategory3")
-        subCategory3.add("SubCategory4")
-
-        val subCategory4: ArrayList<String> = ArrayList()
-        subCategory4.add("SubCategory1")
-        subCategory4.add("SubCategory2")
-        subCategory4.add("SubCategory3")
-        subCategory4.add("SubCategory4")
-
-        this.subCategories[categories[0]] = subCategory1
-        this.subCategories[categories[1]] = subCategory2
-        this.subCategories[categories[2]] = subCategory3
-        this.subCategories[categories[3]] = subCategory4
     }
 
     private fun displayEventList() {
@@ -117,10 +82,10 @@ class ActivityMain: AppCompatActivity(), ExpandableListView.OnChildClickListener
         eventListFragmentTransaction.replace(R.id.relativeLayout, fragment)
         eventListFragmentTransaction.commit()
 
-        favoritesButt.visibility = View.VISIBLE
+        myButton.text = "Favoris"
     }
 
-    fun goToFavorites(view: View) {
+    fun goToFavorites() {
         displayFavorites()
     }
 
@@ -133,7 +98,7 @@ class ActivityMain: AppCompatActivity(), ExpandableListView.OnChildClickListener
         favoritesListFragmentTransaction.replace(R.id.relativeLayout, fragment)
         favoritesListFragmentTransaction.commit()
 
-        favoritesButt.visibility = View.GONE
+        myButton.text = "Events"
     }
 
     override fun onChildClick(
